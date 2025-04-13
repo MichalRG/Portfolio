82fab718c3eefd95913c2fdc0f0e87475c5a77be
Michal <krzyzowskim1998@gmail.com>
2025-04-13 14:01:38 +0200

    FEAT: created base structure of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './componenets/about/about.component';
+import { ContactComponent } from './componenets/contact/contact.component';
+import { FooterComponent } from './componenets/footer/footer.component';
+import { HeaderComponent } from './componenets/header/header.component';
+import { HeroComponent } from './componenets/hero/hero.component';
+import { ProjectsComponent } from './componenets/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [
+    HeaderComponent,
+    HeroComponent,
+    ContactComponent,
+    FooterComponent,
+    AboutComponent,
+    ProjectsComponent,
+  ],
 })
 export class AppComponent {
-  title = 'portfolio-website';
+  title = 'Michal';
 }
