d3d58a6523f32bf46c70012fa8ebaa7cd4d9ed81
Michal <krzyzowskim1998@gmail.com>
2025-04-27 13:36:23 +0200

    FEAT: added context for projects

--- src/app/componenets/projects/projects.component.ts
+++ src/app/componenets/projects/projects.component.ts
@@ -1,11 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { ScrollRevealDirective } from '../../directives/scroll-reveal.directive';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule, TranslateModule, ScrollRevealDirective],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      title: 'PROJECTS.UNIVERSITY.NAME',
+      icon: 'university.svg',
+      description: 'PROJECTS.UNIVERSITY.DESCRIPTION',
+      linkDescription: 'PROJECTS.UNIVERSITY.LINK',
+      link: 'https://ubb.edu.pl/en/',
+    },
+    {
+      title: 'PROJECTS.CERT.NAME',
+      icon: 'certificates.svg',
+      description: 'PROJECTS.CERT.DESCRIPTION',
+      linkDescription: 'PROJECTS.CERT.LINK',
+      link: '#',
+    },
+    {
+      title: 'PROJECTS.GITHUB.NAME',
+      icon: 'github.svg',
+      description: 'PROJECTS.GITHUB.DESCRIPTION',
+      linkDescription: 'PROJECTS.GITHUB.LINK',
+      link: 'https://github.com/MichalRG',
+    },
+    {
+      title: 'PROJECTS.EXPERIENCE.NAME',
+      icon: 'briefcase.svg',
+      description: 'PROJECTS.EXPERIENCE.DESCRIPTION',
+      link: null,
+    },
+  ];
 }
