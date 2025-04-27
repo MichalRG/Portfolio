7bcf6de7d89bd611a7adaf5b9f1ac3c8c10cf9ce
Michal <krzyzowskim1998@gmail.com>
2025-04-27 15:29:24 +0200

    FEAT: added code contacts and fixed a bit styles for about section

--- src/app/componenets/contact/contact.component.ts
+++ src/app/componenets/contact/contact.component.ts
@@ -1,11 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, TranslateModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  contactForm: FormGroup;
+  currentYear = new Date().getFullYear();
 
+  constructor(
+    private translate: TranslateService,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService,
+  ) {
+    this.contactForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]],
+    });
+  }
+
+  get getFormControls() {
+    return this.contactForm.controls;
+  }
+
+  onSubmit() {
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Contact form value:', this.contactForm.value);
+
+    const title = this.translate.instant('CONTACT.TOAST.TITLE');
+    const message = this.translate.instant('CONTACT.TOAST.MESSAGE');
+
+    this.toastr.success(message, title);
+    this.contactForm.reset();
+  }
 }
