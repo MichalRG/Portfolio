eb182f854bb71dd31609aad86f5d308ace7e7b5b
Michal <krzyzowskim1998@gmail.com>
2025-05-01 11:56:27 +0200

    FEAT: added table with translation for explanation of the code

---
+++ src/app/componenets/layout-playground-page/layout-playground-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-layout-playground-page',
+  imports: [TranslateModule],
+  templateUrl: './layout-playground-page.component.html',
+  styleUrl: './layout-playground-page.component.scss',
+})
+export class LayoutPlaygroundPageComponent {}
